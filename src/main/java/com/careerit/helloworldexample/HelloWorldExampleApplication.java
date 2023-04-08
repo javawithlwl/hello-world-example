81356ff1380114b02095d3cc82b06c7d3750ba9a
javawithlwl <javawithlwl@gmail.com>
2023-04-08 07:00:48 +0530

    Spring Boot Hello World

---
+++ src/main/java/com/careerit/helloworldexample/HelloWorldExampleApplication.java
@@ -0,0 +1,13 @@
+package com.careerit.helloworldexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerit/helloworldexample/HelloWorldExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerit.helloworldexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
